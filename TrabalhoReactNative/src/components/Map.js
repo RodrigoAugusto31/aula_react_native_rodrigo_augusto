7e7a4b4c2a680d55bc782e8a5c1245bea440af0f
RodrigoAugusto31 <r.auc@hotmail.com>
2023-11-17 18:12:25 -0300

    adding user position with geolocation

--- TrabalhoReactNative/src/components/Map.js
+++ TrabalhoReactNative/src/components/Map.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
 import Mapbox, {MarkerView} from '@rnmapbox/maps';
 import { keys } from '../secrets/Keys';
 import { MarkersContext } from '../context/MarkersContext';
 import {TouchableOpacity} from 'react-native';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
 Mapbox.setWellKnownTileServer('Mapbox');
 Mapbox.setAccessToken(keys.MAP_BOX_KEY);
@@ -13,12 +14,32 @@ Mapbox.setAccessToken(keys.MAP_BOX_KEY);
 const Map = ({navigation}) => {
 
   const {mymarkers} = useContext(MarkersContext);
+  
+  const [userLatitude, setUserLatitude] = useState(null);
+  const [userLongitude, setUserLongitude] = useState(null);
+
+  useEffect(() => {
+    getCurrentPosition();
+  }, []); 
+
+  const getCurrentPosition = () => {
+    Geolocation.getCurrentPosition(
+      (pos) => {
+        const latitude = pos.coords.latitude;
+        const longitude = pos.coords.longitude;
+        setUserLatitude(latitude);
+        setUserLongitude(longitude);
+      },
+      (error) => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
+      { enableHighAccuracy: true }
+    );
+  };
 
   return (
     <View>
       <ScrollView style={styles.container}>
         <Mapbox.MapView style={styles.map}>
-          <Mapbox.Camera zoomLevel={12} centerCoordinate={[-43.23021649150746, -22.915567583135044]} />
+          <Mapbox.Camera zoomLevel={12} centerCoordinate={[userLongitude, userLatitude]} />
           {mymarkers.map((mymarker) => (
             <MarkerView key={mymarker.id} coordinate={[mymarker.latitude, mymarker.longitude]}>
               <TouchableOpacity
